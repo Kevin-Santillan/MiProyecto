288da7075be291c85e1e323a6ef0ce5c9e5c74ff
Kevin Santillan <kevin.santillanclaudio.7@gmail.com>
2022-04-24 17:53:52 -0500

    Esto es por GUI

---
+++ pruebita.py
@@ -0,0 +1,6 @@
+lista=[]
+diccionario={'name':'kevin','apellido':'santillan'}
+lista.append(diccionario)
+diccionario={'name':'henry','apellido':'Tirando'}
+lista.append(diccionario)
+print(lista[1])
